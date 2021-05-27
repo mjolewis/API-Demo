d8bcdc83025a5f8c58995217a2e654eec8f339d2
Michael Lewis <mjolewis@gmail.com>
2021-05-26 20:22:08 -0400

    CHORE: Removed dead imports

--- src/test/java/com/taurinustrading/dogapi/web/DogControllerIntegrationTest.java
+++ src/test/java/com/taurinustrading/dogapi/web/DogControllerIntegrationTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
